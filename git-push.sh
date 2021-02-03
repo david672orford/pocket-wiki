9f84833b548d709b71556dc88b99a03b2d5424fa
David Chappell <David.Chappell@trincoll.edu>
2021-02-03 16:02:51 -0500

    added script to push wiki pages to a git repository

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,10 @@
 #! /bin/sh -e
 cd `dirname $0`/instance
+if [ ! -d .git ]
+	then
+	echo "Instance directory is not a Git repository."
+	exit 1
+	fi
 git add -A
 git commit -a -m 'auto commit'
 git push
